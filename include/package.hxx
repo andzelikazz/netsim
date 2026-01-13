8a7e3f4cfb832ffba1f3894c13d5e7b6d300a3bf
Angelika <andzelikazuba@gmail.com>
2026-01-13 23:30:48 +0100

    dodano pliki klasy Package

--- include/package.hxx
+++ include/package.hxx
@@ -1,5 +1,24 @@
+#ifndef PACKAGE_HPP
+#define PACKAGE_HPP
+
 #include "types.hxx"
+#include <set>
 
 class Package {
-    
+public:
+	Package();
+	Package(ElementID ID_) : ID(ID_) { assigned_IDs.insert(ID_); };
+	Package(Package&& package) : ID(package.ID) {
+		assigned_IDs.erase(package.ID);
+		freed_IDs.insert(package.ID);
+		package.ID = ElementID{};
+	};
+	Package& operator = (Package&& other) noexcept;
+	ElementID get_id() const { return ID;};
+	~Package(); 
+private:
+	ElementID ID;
+	static std::set<ElementID> assigned_IDs;
+	static std::set<ElementID> freed_IDs;
 };
+#endif 
--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,40 @@
 #include "package.hxx"
+
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
+
+
+Package::Package() {
+    if (!freed_IDs.empty()) {
+        ID = *freed_IDs.begin();
+        freed_IDs.erase(freed_IDs.begin());
+    }
+    else if (assigned_IDs.empty()) {
+        ID = ElementID{1}; 
+    }
+    else {
+        ID = *assigned_IDs.rbegin() + 1;
+    }
+
+    assigned_IDs.insert(ID);
+}
+    
+Package::~Package() {
+    freed_IDs.insert(ID);
+    assigned_IDs.erase(ID);
+}
+
+Package& Package::operator=(Package&& other) noexcept {
+    if (this == &other)
+        return *this;
+
+    assigned_IDs.erase(this->ID);
+    freed_IDs.insert(this->ID);
+
+    this->ID = other.ID;
+    assigned_IDs.insert(this->ID);
+
+    other.ID = ElementID{};
+
+    return *this;
+}
