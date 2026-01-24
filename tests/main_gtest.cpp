5c640e494eabd4cccc8a97d026bb8c2ade501535
Angelika <andzelikazuba@gmail.com>
2026-01-24 21:10:19 +0100

    poprawa cmake

--- tests/main_gtest.cpp
+++ tests/main_gtest.cpp
@@ -1,8 +1,6 @@
 #include "gtest/gtest.h"
-#include "gmock/gmock.h"
 
-int main_tests(int argc, char** argv) {
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
