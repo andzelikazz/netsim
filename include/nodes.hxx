dba1c55a5072985da21496bff51f0d197e82c893
Agata Wójcik <agatakw88@outlook.com>
2026-01-24 20:31:24 +0100

    Refactor ReceiverPreferences and PackageSender classes

--- include/nodes.hxx
+++ include/nodes.hxx
@@ -1,142 +1,76 @@
-#ifndef NODES_HPP_
-#define NODES_HPP_
+#include "nodes.hxx"
 
-#include <map>
-#include <memory>
-#include <optional>
+void ReceiverPreferences::add_receiver(IPackageReceiver *r) {
+  if (!r)
+    return;
+  preferences_[r] = 1.0f;
+  normalize();
+}
 
-#include "helpers.hxx"
-#include "storage_types.hxx"
-#include "types.hxx"
+void ReceiverPreferences::remove_receiver(IPackageReceiver *r) {
+  preferences_.erase(r);
+  normalize();
+}
 
-//enum class ReceiverType { WORKER, STOREHOUSE };
+IPackageReceiver *ReceiverPreferences::choose_receiver() {
+  if (preferences_.empty())
+    return nullptr;
 
-//enum class NodeColor {UNVISITED, VISITED, VERIFIED};
+  double p = pg_();
+  double cumulative = 0.0;
+  for (auto &[receiver, prob] : preferences_) {
+    cumulative += prob;
+    if (p <= cumulative)
+      return receiver;
+  }
+  return preferences_.rbegin()->first;
+}
 
-class IPackageReceiver {
-public:
-  virtual void receive_package(Package &&p) = 0;
-  virtual ElementID get_id() const = 0;
-  //virtual ReceiverType get_receiver_type() const = 0;
+void ReceiverPreferences::normalize() {
+  if (preferences_.empty())
+    return;
 
-  virtual IPackageStockpile::const_iterator begin() const = 0;
-  virtual IPackageStockpile::const_iterator end() const = 0;
-  virtual IPackageStockpile::const_iterator cbegin() const = 0;
-  virtual IPackageStockpile::const_iterator cend() const = 0;
+  float sum = 0.0f;
+  for (auto &[_, prob] : preferences_)
+    sum += prob;
+  if (sum == 0.0f) {
+    float equal_prob = 1.0f / preferences_.size();
+    for (auto &[_, prob] : preferences_)
+      prob = equal_prob;
+  } else {
+    for (auto &[_, prob] : preferences_)
+      prob /= sum;
+  }
+}
 
-  virtual ~IPackageReceiver() = default;
-};
+void PackageSender::send_package() {
+  if (!buffer_) {
+    return; 
+  }
+  IPackageReceiver *receiver = receiver_preferences_.choose_receiver();
+  if(!receiver) return;
+  receiver->receive_package(std::move(*buffer_));
+  buffer_.reset();
+}
 
-class ReceiverPreferences {
-public:
-  using preferences_t = std::map<IPackageReceiver *, double>;
-  using const_iterator = preferences_t::const_iterator;
+void Ramp::deliver_goods(Time t) {
+  if ((t-1)%di_ == 0) {
+      if (!buffer_){
+          push_package(Package(id_));
+      }
+  }
+}
 
-  explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator)
-      : pg_(std::move(pg)) {}
-  void add_receiver(IPackageReceiver *r);
-  void remove_receiver(IPackageReceiver *r);
-  IPackageReceiver *choose_receiver();
-  const preferences_t &get_preferences() const { return preferences_; };
+void Worker::receive_package(Package &&p) { q_->push(std::move(p)); }
 
-  const_iterator begin() const { return preferences_.begin(); };
-  const_iterator end() const { return preferences_.end(); };
-  const_iterator cbegin() const { return preferences_.cbegin(); };
-  const_iterator cend() const { return preferences_.cend(); };
+void Worker::do_work(Time t) {
+    if (!processing_buffer_ && !q_->empty()) {
+        processing_buffer_ = q_->pop();
+        t_ = t;
+    }
 
-private:
-  preferences_t preferences_;
-  ProbabilityGenerator pg_;
-
-  void normalize();
-};
-
-class PackageSender {
-public:
-  PackageSender() = default;
-  PackageSender(PackageSender &&) = default;
-  void send_package();
-  const std::optional<Package> &get_sending_buffer() const { return buffer_; }
-
-  ReceiverPreferences receiver_preferences_;
-
-protected:
-  void push_package(Package &&p) { buffer_.emplace(std::move(p)); }
-  std::optional<Package> buffer_;
-};
-
-class Ramp : public PackageSender {
-public:
-  Ramp(ElementID id, TimeOffset di) : PackageSender(),  id_(id), di_(di) {};
-  void deliver_goods(Time t);
-  TimeOffset get_delivery_interval() const { return di_; };
-  ElementID get_id() const { return id_; };
-
-private:
-  ElementID id_;
-  TimeOffset di_;
-  Time t_ = 0;
-};
-
-class Worker : public PackageSender, public IPackageReceiver {
-public:
-  Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
-      : id_(id), pd_(pd), q_(std::move(q)) {}
-
-  void do_work(Time t); 
-  void receive_package(Package &&p) override; 
-  ElementID get_id() const override { return id_; }
-  //ReceiverType get_receiver_type() const override {
-  //  return ReceiverType::WORKER;
-  //};
-  IPackageStockpile::const_iterator begin() const override {
-    return q_->begin();
-  };
-  IPackageStockpile::const_iterator end() const override { return q_->end(); };
-  IPackageStockpile::const_iterator cbegin() const override {
-    return q_->cbegin();
-  };
-  IPackageStockpile::const_iterator cend() const override {
-    return q_->cend();
-  };
-
-  TimeOffset get_processing_duration() const { return pd_; };
-  Time get_package_processing_start_time() const {return t_;};
-
-private:
-  ElementID id_;
-  TimeOffset pd_;
-  Time t_ = 0;
-  std::unique_ptr<IPackageQueue> q_;
-  std::optional<Package> processing_buffer_;
-};
-
-class Storehouse : public IPackageReceiver {
-public:
-  Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d)
-      : id_(id), d_(std::move(d)) {}
-  explicit Storehouse(ElementID id): id_(id), d_(std::make_unique<PackageQueue>(PackageQueueType::FIFO)){}
-  
-  void receive_package(Package &&p) override { d_->push(std::move(p)); }
-  ElementID get_id() const override { return id_; }
-
-  //ReceiverType get_receiver_type() const override {
-  //return ReceiverType::STOREHOUSE;
-  //};
-  IPackageStockpile::const_iterator begin() const override {
-    return d_->begin();
-  };
-  IPackageStockpile::const_iterator end() const override { return d_->end(); };
-  IPackageStockpile::const_iterator cbegin() const override {
-    return d_->cbegin();
-  };
-  IPackageStockpile::const_iterator cend() const override {
-    return d_->cend();
-  };
-
-private:
-  ElementID id_;
-  std::unique_ptr<IPackageStockpile> d_;
-};
-
-#endif /*NODES_HPP_*/
+    if (processing_buffer_ && (t - t_ + 1>= pd_)) {
+        push_package(std::move(*processing_buffer_));
+        processing_buffer_.reset();
+    }
+}
