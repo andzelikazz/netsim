2e9938a6e4623a6fddfcd55b3ac8c85fcf9262cd
Agata Wójcik <agatakw88@outlook.com>
2026-01-24 19:54:14 +0100

    Comment out receiver type enums and methods

--- include/nodes.hxx
+++ include/nodes.hxx
@@ -9,15 +9,15 @@
 #include "storage_types.hxx"
 #include "types.hxx"
 
-enum class ReceiverType { WORKER, STOREHOUSE };
+//enum class ReceiverType { WORKER, STOREHOUSE };
 
-enum class NodeColor {UNVISITED, VISITED, VERIFIED};
+//enum class NodeColor {UNVISITED, VISITED, VERIFIED};
 
 class IPackageReceiver {
 public:
   virtual void receive_package(Package &&p) = 0;
   virtual ElementID get_id() const = 0;
-  virtual ReceiverType get_receiver_type() const = 0;
+  //virtual ReceiverType get_receiver_type() const = 0;
 
   virtual IPackageStockpile::const_iterator begin() const = 0;
   virtual IPackageStockpile::const_iterator end() const = 0;
@@ -61,13 +61,13 @@ public:
   ReceiverPreferences receiver_preferences_;
 
 protected:
-  void push_package(Package &&p) { buffer_.emplace(p.get_id()); }
+  void push_package(Package &&p) { buffer_.emplace(std::move(p)); }
   std::optional<Package> buffer_;
 };
 
 class Ramp : public PackageSender {
 public:
-  Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {};
+  Ramp(ElementID id, TimeOffset di) : PackageSender(),  id_(id), di_(di) {};
   void deliver_goods(Time t);
   TimeOffset get_delivery_interval() const { return di_; };
   ElementID get_id() const { return id_; };
@@ -75,7 +75,7 @@ public:
 private:
   ElementID id_;
   TimeOffset di_;
-  Time t_;
+  Time t_ = 0;
 };
 
 class Worker : public PackageSender, public IPackageReceiver {
@@ -83,12 +83,12 @@ public:
   Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
       : id_(id), pd_(pd), q_(std::move(q)) {}
 
-  void do_work(Time t); //
-  void receive_package(Package &&p) override; //
+  void do_work(Time t); 
+  void receive_package(Package &&p) override; 
   ElementID get_id() const override { return id_; }
-  ReceiverType get_receiver_type() const override {
-    return ReceiverType::WORKER;
-  };
+  //ReceiverType get_receiver_type() const override {
+  //  return ReceiverType::WORKER;
+  //};
   IPackageStockpile::const_iterator begin() const override {
     return q_->begin();
   };
@@ -115,13 +115,14 @@ class Storehouse : public IPackageReceiver {
 public:
   Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d)
       : id_(id), d_(std::move(d)) {}
-
+  explicit Storehouse(ElementID id): id_(id), d_(std::make_unique<PackageQueue>(PackageQueueType::FIFO)){}
+  
   void receive_package(Package &&p) override { d_->push(std::move(p)); }
   ElementID get_id() const override { return id_; }
 
-  ReceiverType get_receiver_type() const override {
-    return ReceiverType::STOREHOUSE;
-  };
+  //ReceiverType get_receiver_type() const override {
+  //return ReceiverType::STOREHOUSE;
+  //};
   IPackageStockpile::const_iterator begin() const override {
     return d_->begin();
   };
