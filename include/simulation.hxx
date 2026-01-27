168593c465a1621a0529b3d5dce326e46a17fa76
Apoloniia <agatakw88@outlook.com>
2026-01-27 21:54:38 +0100

    funkcje jednolijikowe zaimplementowane

--- include/simulation.hxx
+++ include/simulation.hxx
@@ -6,26 +6,29 @@
 #include <functional>
 #include <set>
 
-
-void simulate(Factory& f, TimeOffset duration, std::function<void(Factory&, Time)> rf);
+void simulate(Factory &f, TimeOffset duration,
+              std::function<void(Factory &, Time)> rf);
 
 class SpecificTurnsReportNotifier {
 public:
-    SpecificTurnsReportNotifier(std::set<Time> turns);
-    bool should_generate_report(Time t) const;
+  SpecificTurnsReportNotifier(std::set<Time> turns) : turns_(turns) {};
+  bool should_generate_report(Time t) const {
+    return turns_.find(t) != turns_.cend();
+  };
 
 private:
-    std::set<Time> turns_;
+  std::set<Time> turns_;
 };
-
 
 class IntervalReportNotifier {
 public:
-    IntervalReportNotifier(TimeOffset interval);
-    bool should_generate_report(Time t) const;
+  IntervalReportNotifier(TimeOffset interval) : interval_(interval) {};
+  bool should_generate_report(Time t) const {
+    return t == 1 || t % interval_ == 1;
+  };
 
 private:
-    TimeOffset interval_;
+  TimeOffset interval_;
 };
 
-#endif 
+#endif
