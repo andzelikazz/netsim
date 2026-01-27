3fc04427b75d208d27cbb6460fac4f718fe0f6f5
Apoloniia <agatakw88@outlook.com>
2026-01-27 22:18:05 +0100

    deklaracja funckcji

---
+++ include/reports.hxx
@@ -0,0 +1,10 @@
+#ifndef REPORTS_HPP_
+#define REPORTS_HPP_
+
+#include "factory.hxx"
+
+void generate_structure_report(const Factory &factory, std::ostream &os);
+
+void generate_simulation_turn_report(const Factory &factory, std::ostream &os,
+                                     Time t);
+#endif // !DEBUG
