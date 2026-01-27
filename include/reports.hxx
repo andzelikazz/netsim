ce6965ba46a3f4964e7b49cbccb3201ac5ea665d
Apoloniia <agatakw88@outlook.com>
2026-01-27 22:57:35 +0100

    zmiana naazwy zarhumentu funkcji na krotsza

--- include/reports.hxx
+++ include/reports.hxx
@@ -3,8 +3,8 @@
 
 #include "factory.hxx"
 
-void generate_structure_report(const Factory &factory, std::ostream &os);
+void generate_structure_report(const Factory &f, std::ostream &os);
 
-void generate_simulation_turn_report(const Factory &factory, std::ostream &os,
+void generate_simulation_turn_report(const Factory &f, std::ostream &os,
                                      Time t);
 #endif // !DEBUG
